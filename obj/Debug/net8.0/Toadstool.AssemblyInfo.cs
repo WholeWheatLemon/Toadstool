334f9c935abf745167c8d25def9e2fbf32738bc5
ollie <me@ollie.lol>
2026-02-11 16:27:34 -0500

    removed scene folder

--- obj/Debug/net8.0/Toadstool.AssemblyInfo.cs
+++ obj/Debug/net8.0/Toadstool.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Toadstool")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c73c86eb3ece94f5b53dbda66407b7e163f8fb43")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+151e50cdbda5d403b3c88d242ace16562b294302")]
 [assembly: System.Reflection.AssemblyProductAttribute("Toadstool")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Toadstool")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
