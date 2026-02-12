b5e317ceb3da657ac68544715e41574349945dcd
ollie <me@ollie.lol>
2026-02-12 00:09:10 -0500

    more things

--- Input/InputManager.cs
+++ Input/InputManager.cs
@@ -1,6 +1,36 @@
+using Microsoft.Xna.Framework;
+
 namespace Toadstool.Input;
 
 public class InputManager
 {
-    
+    /// <summary>
+    /// Gets the state information of keyboard input.
+    /// </summary>
+    public KeyboardInfo Keyboard { get; private set; }
+
+    /// <summary>
+    /// Gets the state information of mouse input.
+    /// </summary>
+    public MouseInfo Mouse { get; private set; }
+
+    /// <summary>
+    /// Gets the state information of a gamepad.
+    /// </summary>
+    public GamePadInfo[] GamePads { get; private set; }
+
+    /// <summary>
+    /// Creates a new InputManager.
+    /// </summary>
+    public InputManager()
+    {
+        Keyboard = new KeyboardInfo();
+        Mouse = new MouseInfo();
+
+        GamePads = new GamePadInfo[4];
+        for (int i = 0; i < 4; i++)
+        {
+            GamePads[i] = new GamePadInfo((PlayerIndex)i);
+        }
+    }
 }
--- obj/Debug/net8.0/Toadstool.AssemblyInfo.cs
+++ obj/Debug/net8.0/Toadstool.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Toadstool")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+151e50cdbda5d403b3c88d242ace16562b294302")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+334f9c935abf745167c8d25def9e2fbf32738bc5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Toadstool")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Toadstool")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
